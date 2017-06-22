7b27694e27da0e14329eb8d2d18d26f8913b1b0b
wellington <wchevreuil@cloudera.com>
2017-06-22 17:42:24 +0100

    removed wait() calls that are now irrelevant. Added logging

--- src/main/java/dfsclient/test/DFSClientTest.java
+++ src/main/java/dfsclient/test/DFSClientTest.java
@@ -6,9 +6,11 @@ import java.io.OutputStream;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.log4j.Logger;
 
 public class DFSClientTest {
 
+  private static Logger log = Logger.getLogger(DFSClientTest.class);
   /**
    * @param args
    */
@@ -22,33 +24,38 @@ public class DFSClientTest {
 
       int i = 0;
 
-      OutputStream os = new BufferedOutputStream(fs.create(new Path("/test/my-test10")));
+      OutputStream os = new BufferedOutputStream(fs.create(new Path(args[0])));
 
-      while (i < (50 * 1024 * 1024)) {
+      while (i < Integer.parseInt(args[1])) {
 
         System.out.println(" waiting before write test " + i);
 
-        synchronized (os) {
-          os.wait(70000);
-        }
+        log.trace(" waiting before write test " + i);
+        log.debug(" waiting before write test " + i);
+        log.info(" waiting before write test " + i);
+
+        // synchronized (os) {
+        // os.wait(70000);
+        // }
 
         os.write("test".getBytes(), 0, "test".getBytes().length);
 
         System.out.println("wrote test " + i);
-
-        synchronized (os) {
-          os.wait(70000);
-        }
+        //
+        // synchronized (os) {
+        // os.wait(70000);
+        // }
         i++;
-
-        os.flush();
 
       }
 
+      os.flush();
       // is.close();
       // fs.close();
       fs.close();
+
       os.close();
+
     } catch (Exception e) {
       e.printStackTrace();
     }
